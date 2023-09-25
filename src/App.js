f50843efbd31102d169bd03c7273417be235e58d
Prashanth Kumar <prashanth.sunny0236@gmail.com>
2023-09-25 16:01:11 +0530

    CSS changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { useEffect, useState } from 'react';
-import { Button, TextField, Snackbar, Alert, Tooltip}  from '@mui/material';
+import { Button, TextField, Snackbar, Alert, Tooltip } from '@mui/material';
 import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
-import { getMessage, saveMessage, checkServerStatus} from './services/message.service';
+import DoneRoundedIcon from '@mui/icons-material/DoneRounded';
+import ClearRoundedIcon from '@mui/icons-material/ClearRounded';
+import CachedRoundedIcon from '@mui/icons-material/CachedRounded';
+import { getMessage, saveMessage, checkServerStatus } from './services/message.service';
 import './App.css';
 
-function debounce(func, timeout = 500){
+function debounce(func, timeout = 500) {
   let timer;
   return (...args) => {
     clearTimeout(timer);
@@ -13,12 +16,13 @@ function debounce(func, timeout = 500){
 }
 
 let toastMessage = '';
-let severity='success';
+let severity = 'success';
 //let alertProps = {message: '', severity: 'success'};
 let clearPollTimer;
 const alertSuccessMessage = 'Server running...';
 const alertErrorMessage = `Server down. When accessed for the first time after being inactive for more than 15 minutes, it takes mostly 15s to start up. Polling every 5secs to check status. 
-Please wait and DONT reload if the server is not up for more than 5 minutes...`;
+Please wait and DONT reload unless the server is not up for more than 5 minutes...`;
+
 function App() {
   const [message, setMessage] = useState('');
   const [messageTime, setMessageTime] = useState('');
@@ -28,20 +32,15 @@ function App() {
   const [serverUp, setServerUp] = useState(false);
 
   useEffect(() => {
-    if(serverUp) {
-      getMessage().then(res => {
-         setMessage(res.data.message);
-         setMessageTime(res.data.time);
-         setPrevMessage({message: res.data.previousMessage, time: res.data.previousMessageTime});
-         if(clearPollTimer) clearInterval(clearPollTimer);
-      })
+    if (serverUp) {
+      getLatestMessage();
     }
   }, [serverUp])
 
 
   useEffect(() => {
-    if(serverUp) {
-      if(clearPollTimer) {
+    if (serverUp) {
+      if (clearPollTimer) {
         clearInterval(clearPollTimer);
       }
     } else {
@@ -56,32 +55,41 @@ function App() {
 
   const checkServer = () => {
     checkServerStatus().then(res => {
-      if(res.status === 200 && res.data === 'working') {
-        if(!serverUp) setServerUp(true);
+      if (res.status === 200 && res.data === 'working') {
+        if (!serverUp) setServerUp(true);
       }
     }).catch(e => {
-     if(serverUp) setServerUp(false);
+      if (serverUp) setServerUp(false);
+    })
+  }
+
+  const getLatestMessage = () => {
+    getMessage().then(res => {
+      setMessage(res.data.message);
+      setMessageTime(res.data.time);
+      setPrevMessage({ message: res.data.previousMessage, time: res.data.previousMessageTime });
+      if (clearPollTimer) clearInterval(clearPollTimer);
     })
   }
 
   const save = e => {
     saveMessage(message).then(res => {
-      if(res.status === 200) {
+      if (res.status === 200) {
         toastMessage = 'Save successful';
-        severity='success';
+        severity = 'success';
       } else {
-        toastMessage="Couldn't save";
-        severity='error';
+        toastMessage = "Couldn't save";
+        severity = 'error';
       }
       openToast();
     }).catch(e => {
-      toastMessage=e.message;
-      severity="error";
+      toastMessage = e.message;
+      severity = "error";
       openToast();
     })
   }
 
-  const clear= () => {
+  const clear = () => {
     setMessage('');
     //save();
   }
@@ -95,11 +103,11 @@ function App() {
   };
 
   const handleClose = (event, reason) => {
-      if (reason === 'clickaway') {
-        return;
-      }
+    if (reason === 'clickaway') {
+      return;
+    }
 
-      setOpen(false);
+    setOpen(false);
   };
 
   const getTime = time => {
@@ -117,47 +125,57 @@ function App() {
 
 
   return (
-   <section className='app-ctr'>
-    <section className='top-bar'>
-      {serverUp !== '' && <Alert sx={{width: '55%'}} severity={serverUp ? 'success' : 'info'}>{serverUp ? alertSuccessMessage :  alertErrorMessage}</Alert>}
-    </section>
-    <section className='previous-message-ctr'>
-      <section className='previous-message-header-ctr'>
-        {prevMessageObj.message && <p>Previous Message:</p>}
-        <span style={{display: 'flex'}}>
-          <Tooltip title="8919368035" placement="left" arrow>
-            <InfoOutlinedIcon />
-          </Tooltip>
-        {prevMessageObj.time && <span class="message-time-ctr">{getTime(prevMessageObj.time)}</span>}
-        </span>
+    <section className='app-ctr'>
+      <section className='top-bar'>
+        {serverUp !== '' && <Alert sx={{ width: '55%' }} severity={serverUp ? 'success' : 'info'}>{serverUp ? alertSuccessMessage : alertErrorMessage}</Alert>}
       </section>
-      {prevMessageObj.message && <section className='previous-message-content-ctr'>{prevMessageObj.message}</section>}
-    </section>
+      <section className='previous-message-ctr'>
+        <section className='previous-message-header-ctr'>
+          {prevMessageObj.message && <p className='previous-message-label'>Previous Message:</p>}
+          <span style={{ display: 'flex' }}>
+            <Tooltip title="8919368035" placement="left" arrow>
+              <InfoOutlinedIcon />
+            </Tooltip>
+            {prevMessageObj.time && <span class="message-time-ctr">{getTime(prevMessageObj.time)}</span>}
+          </span>
+        </section>
+        {prevMessageObj.message && <section className='previous-message-content-ctr'>{prevMessageObj.message}</section>}
+      </section>
       <section className='message-ctr'>
-          <section className='message-field-ctr'>
-            {messageTime && <span class="message-time-ctr">Date Time: {getTime(messageTime)}</span>}
-              <TextField
-                   multiline
-                   rows={10}
-                   fullWidth 
-                   label="Message"
-                   placeholder="Tell me anything here"
-                   value={message}
-                   onChange={messageChanged}
-              
-              />
-          </section>
-          <section className='message-btn-ctr'>
-                <Button variant='outlined' className='clear-btn' onClick={clear}>Clear</Button>
-                <Button variant='contained' className='save-btn' onClick={save}>Save</Button>
-          </section>
+        <section className='message-field-ctr'>
+          {messageTime && <span class="message-time-ctr">{getTime(messageTime)}</span>}
+          <TextField
+            multiline
+            rows={10}
+            fullWidth
+            label="Message"
+            placeholder="Tell me anything here"
+            value={message}
+            onChange={messageChanged}
+
+          />
+        </section>
+        <section className='message-btn-ctr'>
+          <Button variant='outlined' className='refresh-btn footer-btn' onClick={getLatestMessage}>
+            <CachedRoundedIcon />
+            <span>Refresh</span>
+          </Button>
+          <Button variant='outlined' className='clear-btn footer-btn' onClick={clear}>
+            <ClearRoundedIcon />
+            <span>Clear</span>
+          </Button>
+          <Button variant='contained' className='save-btn footer-btn' onClick={save}>
+            <DoneRoundedIcon />
+            <span>Save</span>
+          </Button>
+        </section>
       </section>
-      <Snackbar open={open} autoHideDuration={5000} onClose={handleClose} anchorOrigin={{vertical: 'bottom', horizontal: 'center'}}>
+      <Snackbar open={open} autoHideDuration={5000} onClose={handleClose} anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}>
         <Alert onClose={handleClose} severity={severity} sx={{ width: '100%' }}>
-         {toastMessage}
+          {toastMessage}
         </Alert>
       </Snackbar>
-   </section>
+    </section>
   )
 }
 
