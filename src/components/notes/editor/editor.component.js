295ab46917e558b369cb326a5dbb3c8cefc9950e
PT-CTS <youremail@example.com>
2024-04-17 23:22:57 +0530

    Add Save button to Note

--- src/components/notes/editor/editor.component.js
+++ src/components/notes/editor/editor.component.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import {  Button } from '@mui/material';
+import DoneRoundedIcon from '@mui/icons-material/DoneRounded';
 import './editor.component.css';
 
 function debounce(func, timeout = 2000) {
     let timer;
     return (...args) => {
+      console.log(args);
       clearTimeout(timer);
       timer = setTimeout(() => { func.apply(this, args); }, timeout);
     };
@@ -15,6 +18,10 @@ function NotesEditor(props) {
   const { currentNote, saveNoteContent } = props;
   let notesEditorObj = null;
   const debounceSaveCurrentNote = debounce(saveNoteContent);
+
+  const saveNoteHandler = () => {
+    notesEditorObj && debounceSaveCurrentNote({...currentNote, content: notesEditorObj.getData()});
+  }
   return (
     <section className="editor">
         <CKEditor
@@ -24,12 +31,17 @@ function NotesEditor(props) {
                 notesEditorObj = editor;
             } }
             onChange={ ( event ) => {
-                debounceSaveCurrentNote({...currentNote, content: notesEditorObj?.getData() || currentNote.content});
+                
             } }
             onBlur={ ( event, editor ) => {
                 console.log( 'Blur.', editor );
             } }
         />
+
+        <Button variant='contained' className='save-note-btn' onClick={saveNoteHandler}>
+          <DoneRoundedIcon />
+          <span>Add</span>
+        </Button>
     </section>
   );
 }
