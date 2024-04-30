9965bcb5e7bf0cbfdb2465ed7007c9a29af4adb8
PrashanthLM10 <prashanth.sunny0236@gmail.com>
2024-04-30 15:57:17 +0530

    Update to display Mask

--- src/App.js
+++ src/App.js
@@ -8,10 +8,6 @@ function App() {
   const router = createBrowserRouter([
     {
       path: '/',
-      element: <Messages />
-    },
-    {
-      path: '/mask',
       element: <ComponentResolver />
     },
     {
