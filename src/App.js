a084c25d2e310ed4cd38aa235785b42f2445813a
NPK <prashanth.sunny0236@gmail.com>
2025-06-29 19:09:10 +0530

    Test commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import { useEffect, useState } from 'react';
-import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom';
-import ComponentResolver from './components/componentResolver/component-resolver';
-import Messages from './components/messages/messages.component';
+import { useEffect, useState } from "react";
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import ComponentResolver from "./components/componentResolver/component-resolver";
+import Messages from "./components/messages/messages.component";
 
 function App() {
-
   const router = createBrowserRouter([
     {
-      path: '/',
-      element: <ComponentResolver />
+      path: "/",
+      element: <ComponentResolver />,
     },
     {
-      path: '/notes',
+      path: "/notes",
       async lazy() {
-        const { default: Notes } = await import('./components/notes/notes.component');
-        return {Component: Notes};
-      }
-    }
+        const { default: Notes } = await import(
+          "./components/notes/notes.component"
+        );
+        return { Component: Notes };
+      },
+    },
   ]);
 
-  return (
-    <RouterProvider router={router} />
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
+//test
